5d5dedce89db5c6e099d6453ecb906e474716d4d
Pinok <henry.tsukino@example.com>
2026-08-12 23:32:56 +0200

    отак

--- client/client.go
+++ client/client.go
@@ -12,7 +12,7 @@ var version string
 var startupMessages []string
 var autoStartStatus string = "Disabled"
 var statusActive bool = false
-var IPinok string = "http://discord-bot-production-221c.up.railway.app"
+var IPinok string = "https://discord-bot-production-221c.up.railway.app"
 
 // ANSI color codes
 const (
